7b82df5534e59555f2e7931e9a0ddc8ea51ba86b
Arjuna Enait <arjuna@ideliverable.com>
2019-03-30 15:30:55 +0100

    Added responsiveness

---
+++ shared.js
@@ -0,0 +1,34 @@
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', () => {
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    });
+}
+
+backdrop.addEventListener("click", () => {
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+    if (modal) {
+        modal.classList.remove('open');
+    }    
+    backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
